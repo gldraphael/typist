aa91cdf7888e0f699c6167c90c22c1ffed1bdb6e
Galdin Raphael <gldraphael@users.noreply.github.com>
2021-05-30 13:18:06 +0530

    Get a working typing view

---
+++ src/Typist.WasmApp/EventBus.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using System;
+
+namespace Typist.WasmApp
+{
+    public class EventBus
+    {
+        public event Action<KeyboardEventArgs>? OnKeyDown;
+        public event Action<string>? OnTextInput;
+
+        public void RaiseKeyDownEvent(KeyboardEventArgs args)
+        {
+            OnKeyDown?.Invoke(args);
+        }
+
+        public void RaiseTextInputEvent(string text)
+        {
+            OnTextInput?.Invoke(text);
+        }
+    }
+}
